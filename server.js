d0e4228d4d926137ba0373da900c078d2dbd1163
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-01-23 19:10:48 -0600

    created a route for delete to delete notes, currently only working on page reload

--- server.js
+++ server.js
@@ -46,14 +46,14 @@ app.post('/api/notes', (req, res) => {
 
     // If all the required properties are present
     if (title && text) {
-        // Variable for the object we will save
+        // new object to save data
         const newNote = {
             title,
             text,
             id: uuid()
         };
 
-        // Obtain existing reviews
+        // Obtain existing notes
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err);
@@ -88,6 +88,42 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+    console.log(req.params)
+
+    //reading current list of notes
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // Convert string into JSON object
+            const parsedNotes = JSON.parse(data);
+
+            for (let i = 0; i < parsedNotes.length; i++) {
+
+                if (parsedNotes[i].id == req.params.id) {
+                    // Splice on index position, and then deletes 1 at that index
+                    parsedNotes.splice(i, 1);
+                }
+            }
+
+
+            // Write deleted notes back to the file
+            fs.writeFile(
+                './db/db.json',
+                JSON.stringify(parsedNotes),
+                (writeErr) =>
+                    writeErr
+                        ? console.error(writeErr)
+                        : console.info('Successfully deleted notes!')
+            );
+        }
+    });
+    //this is working however, only on page refresh
+});
+
+
+///listen to port
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
